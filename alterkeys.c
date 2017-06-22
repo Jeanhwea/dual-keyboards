735d1ff905d186c6d3c02caca0a8ecf5b9fd85c1
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-06-22 16:52:05 +0800

    refactoring codes

--- alterkeys.c
+++ alterkeys.c
@@ -3,7 +3,7 @@
 // modified by Chance Miller to support multikeyboard use
 //
 // Complile using the following command line:
-//	 gcc -Wall -o alterkeys alterkeys.c -framework ApplicationServices
+//      gcc -Wall -o alterkeys alterkeys.c -framework ApplicationServices
 //
 // You need superuser privileges to create the event tap, unless accessibility
 // is enabled. To do so, select the "Enable access for assistive devices"
@@ -11,111 +11,71 @@
 
 #include <ApplicationServices/ApplicationServices.h>
 
-//Global Variables to keey track of modifier keys pressed
-bool ctr = false;
-bool sft = false;
-bool cmd = false;
-bool opt = false;
+// Global Variables to keey track of modifier keys pressed
+bool ctrl = false;
+bool shift = false;
+bool command = false;
+bool option = false;
 
 // This callback will be invoked every time there is a keystroke.
-//
-CGEventRef
-myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
-				  CGEventRef event, void *refcon)
+CGEventRef cbCGEvent(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
 {
-	// Paranoid sanity check.
-	if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp) && (type != kCGEventFlagsChanged))
-		return event;
+    // Paranoid sanity check.
+    if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp) && (type != kCGEventFlagsChanged))
+        return event;
 
-	// The incoming keycode.
-	CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
-	//Control
-	if(keycode == (CGKeyCode)59||keycode == (CGKeyCode)62){
-		if(ctr){
-			ctr = false;
-		}
-		else{
-			ctr = true;
-		}
-	}
-	if(ctr){
-		CGEventSetFlags(event,NX_CONTROLMASK|CGEventGetFlags(event));
-	}
-	//Shift
-	if(keycode == (CGKeyCode)60||keycode == (CGKeyCode)56){
-		if(sft){
-			sft = false;
-		}
-		else{
-			sft = true;
-		}
-	}
-	if(sft){
-		CGEventSetFlags(event,NX_SHIFTMASK|CGEventGetFlags(event));
-	}
-	//Command
-	if(keycode == (CGKeyCode)55||keycode == (CGKeyCode)54){
-		if(cmd){
-			cmd = false;
-		}
-		else{
-			cmd = true;
-		}
-	}
-	if(cmd){
-		CGEventSetFlags(event,NX_COMMANDMASK|CGEventGetFlags(event));
-	}
-	//Option
-	if(keycode == (CGKeyCode)58||keycode == (CGKeyCode)61){
-		if(opt){
-			opt = false;
-		}
-		else{
-			opt = true;
-		}
-	}
-	if(opt){
-		CGEventSetFlags(event,NX_ALTERNATEMASK|CGEventGetFlags(event));
-	}
-	CGEventSetIntegerValueField(
-		event, kCGKeyboardEventKeycode, (int64_t)keycode);
+    // The incoming keycode.
+    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+    if (keycode == (CGKeyCode)59 || keycode == (CGKeyCode)62) {
+        ctrl = !ctrl;
+    }
+    if (ctrl) CGEventSetFlags(event, NX_CONTROLMASK | CGEventGetFlags(event));
+    if (keycode == (CGKeyCode)60 || keycode == (CGKeyCode)56) {
+        shift = !shift;
+    }
+    if (shift) CGEventSetFlags(event, NX_SHIFTMASK | CGEventGetFlags(event));
+    if (keycode == (CGKeyCode)55||keycode == (CGKeyCode)54) {
+        command = !command;
+    }
+    if (command) CGEventSetFlags(event, NX_COMMANDMASK | CGEventGetFlags(event));
+    if (keycode == (CGKeyCode)58||keycode == (CGKeyCode)61) {
+        option = !option;
+    }
+    if (option) CGEventSetFlags(event, NX_ALTERNATEMASK | CGEventGetFlags(event));
+    CGEventSetIntegerValueField(event, kCGKeyboardEventKeycode, (int64_t)keycode);
 
-	// We must return the event for it to be useful.
-	return event;
+    // We must return the event for it to be useful.
+    return event;
 }
 
-int
-main(void)
+int main(int argc, char *argv[])
 {
-	CFMachPortRef	  eventTap;
-	CGEventMask		eventMask;
-	CFRunLoopSourceRef runLoopSource;
+    CFMachPortRef eventTap;
+    CGEventMask eventMask;
+    CFRunLoopSourceRef runLoopSource;
 
-	// Create an event tap. We are interested in key presses.
-	eventMask = ((1 << kCGEventKeyDown) | (1 << kCGEventKeyUp) | (1 << kCGEventFlagsChanged));
-	eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
-								eventMask, myCGEventCallback, NULL);
-	if (!eventTap) {
-		fprintf(stderr, "failed to create event tap\n");
-		exit(1);
-	}
+    // Create an event tap. We are interested in key presses.
+    eventMask = ((1 << kCGEventKeyDown) | (1 << kCGEventKeyUp) | (1 << kCGEventFlagsChanged));
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, eventMask, cbCGEvent, NULL);
 
-	// Create a run loop source.
-	runLoopSource = CFMachPortCreateRunLoopSource(
-						kCFAllocatorDefault, eventTap, 0);
+    if (!eventTap) {
+        fprintf(stderr, "failed to create event tap\n");
+        exit(1);
+    }
 
-	// Add to the current run loop.
-	CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource,
-					   kCFRunLoopCommonModes);
+    // Create a run loop source.
+    runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
 
-	// Enable the event tap.
-	CGEventTapEnable(eventTap, true);
+    // Add to the current run loop.
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
 
-	// Set it all running.
-	CFRunLoopRun();
+    // Enable the event tap.
+    CGEventTapEnable(eventTap, true);
 
-	// In a real program, one would have arranged for cleaning up.
+    // Set it all running.
+    CFRunLoopRun();
 
-	exit(0);
+    // In a real program, one would have arranged for cleaning up.
+    return 0;
 }
 
